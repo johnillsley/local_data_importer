f7c519f662136f4a47ab9c9ff7288634628efc37
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-10-12 13:37:20 +0100

    added new connector instance form

--- dataimporterform.php
+++ dataimporterform.php
@@ -14,3 +14,30 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
+require_once($CFG->libdir . '/formslib.php');
+
+class local_data_importer_form extends moodleform {
+    public function definition() {
+        $mform = $this->_form;
+
+        //Connector Name
+        $mform->addElement('text', 'connector_name', get_string('connector_name', 'local_data_importer'));
+        $mform->addRule('connector_name', get_string('required'), 'required', null, 'client');
+        $mform->setType('connector_name', PARAM_TEXT);
+
+        //Connector Description
+        $mform->addElement('textarea', 'connector_description', get_string('connector_description', 'local_data_importer'), 'maxlength="254" size="50"');
+        $mform->addRule('connector_description', get_string('required'), 'required', null, 'client');
+        $mform->setType('connector_description', PARAM_TEXT);
+
+        // Swaggerhub Definition API
+        $mform->addElement('text', 'openapidefinitionurl', get_string('openapidefinitionurl_label', 'local_data_importer'));
+        $mform->addRule('openapidefinitionurl', get_string('required'), 'required', null, 'client');
+        $mform->setType('openapidefinitionurl', PARAM_TEXT);
+
+        // Fetch Definition button
+        $mform->addElement('button', 'fetchapidef', 'Fetch');
+
+        $this->add_action_buttons();
+    }
+}
