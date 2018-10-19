59a6ea79c8ab37ce19e7a91249017492277d6e5c
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-10-19 11:19:44 +0100

    Code reformat

--- version.php
+++ version.php
@@ -22,6 +22,6 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2018100202;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2017110800;        // Requires this Moodle version
-$plugin->component = 'local_data_importer'; // Full name of the plugin (used for diagnostics)
+$plugin->version = 2018100202;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires = 2017110800;        // Requires this Moodle version
+$plugin->component = 'local_data_importer'; // Full name of the plugin (used for diagnostics).
