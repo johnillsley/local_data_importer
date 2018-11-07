b24dab5d33c0819c066039e4cd36eeab0406da54
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-11-07 09:54:48 +0000

    add new connector when form submitted

--- add_connector.php
+++ add_connector.php
@@ -20,24 +20,53 @@ $returnurl = new moodle_url('/local/data_importer/index.php');
 $url = new moodle_url('/local/data_importer/add_connector.php');
 $PAGE->set_url($url);
 $PAGE->set_context(\context_system::instance());
-
+require_login();
 $connectorid = optional_param('connectorid', 0, PARAM_INT);
-if($connectorid){
+$connectordata = null;
+if ($connectorid) {
     // Existing connector.
     $url->param('connectorid', $connectorid);
-}
-require_login();
+    $renderable = new local_data_importer\output\importers_page();
+    $connectordata = $renderable->get_single_connector_instance($connectorid);
+    var_dump($connectordata);
 
+}
+$connectorinstance = new local_data_importer_connectorinstance();
 $mform = new local_data_importer_connector_form(null);
+$mform->set_data($connectordata);
 if ($mform->is_cancelled()) {
     echo "cancelled";
     redirect($returnurl);
 }
-if (!$mform->is_cancelled() && $data = $mform->get_data()) {
-    var_dump($data);
+if (!$mform->is_cancelled() && $formdata = $mform->get_data()) {
+    $server = '';
+    // Additionally get the servers.
+    if (isset($_POST['apiserver'])) {
+        $server = $_POST['apiserver'];
+    }
+    if (!empty($formdata)) {
+        if (isset($_POST['connectorid'])) {
+            $connectorinstance->setid($_POST['connectorid']);
+        }
+        $connectorinstance->setdescription($formdata->description);
+        $connectorinstance->setname($formdata->name);
+        $connectorinstance->set_openapidefinitionurl($formdata->openapidefinitionurl);
+        $connectorinstance->setopenapikey($formdata->openapikey);
+        $connectorinstance->set_server_apikey($formdata->serverapikey);
+        $connectorinstance->setserver($server);
+        try {
+            $retid = $connectorinstance->save(true);
+            // Redirect to index page.
+            redirect($returnurl);
+        } catch (\Exception $e) {
+            $displaynoticebad = $e->getMessage();
+        }
 
+    }
 }
 echo $OUTPUT->header();
+$PAGE->requires->js_call_amd('local_data_importer/fetch_api_definition', 'init', []);
 $mform->display();
+
 
 echo $OUTPUT->footer();
