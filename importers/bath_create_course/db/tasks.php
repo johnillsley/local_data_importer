942e068c632ab13d3be79fb8834ee2ab09663557
Hittesh Ahuja <h.ahuja@bath.ac.uk>
2018-11-19 16:40:32 +0000

    changed version number - added tasks

--- importers/bath_create_course/db/tasks.php
+++ importers/bath_create_course/db/tasks.php
@@ -13,7 +13,14 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2018100206;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires = 2017110800;        // Requires this Moodle version
-$plugin->component = 'importers_bath_create_course'; // Full name of the plugin (used for diagnostics).
+defined('MOODLE_INTERNAL') || die();$tasks = array(
+    array(
+        'classname' => '\importers_bath_create_course\task\create_course_sync_task',
+        'blocking' => 0,
+        'minute' => '15',
+        'hour' => '*',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    )
+);
