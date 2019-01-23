fe2ea29e5d924ec94645b1ad8e88d5aebb6599c1
John Illsley <jsi22@bath.ac.uk>
2019-01-23 15:18:40 +0000

    Merge branch 'develop_jsi22' of https://bitbucket.org/universityofbathelearning/local_moodle_data_importer into develop_jsi22
    # Conflicts:
    #	classes/connectorinstance.php
    #	classes/connectorpathitem.php
    #	index.php

---
+++ tests/scheduler_test.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the local/data_importer/classes/scheduler.php.
+ * Moodle code checker does not like some lines of code with spaces at the end.
+ * Do not remove these spaces or the phpunit tests will fail.
+ * Also be careful that your text/code editor does not automatically strip these trailing spaces when saving.
+ *
+ * @group      local_data_importer
+ * @group      bath
+ * @package    local/data_importer
+ * @author     John Illsley <j.s.illsley@bath.ac.uk>
+ * @copyright  2018 University of Bath
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class local_data_importer_scheduler_test extends advanced_testcase {
+
+    public function test_move_item_up() {
+
+    }
+
+    public function test_move_item_down() {
+
+    }
+
+    public function test_get_last_id() {
+        // Is this name right?
+
+        // Get biggest order number.
+    }
+
+    public function test_start_data_imports() {
+
+    }
+}
